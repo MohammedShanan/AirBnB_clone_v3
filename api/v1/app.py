775a061903721a268a0f3f4740652c7f1792f981
mohammedshanan <mohammedshana12@gmail.com>
2024-08-01 22:47:05 +0300

    added error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+""" a Flask application for an API server """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 
 app = Flask(__name__)
@@ -12,6 +13,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,31 @@
+#!/usr/bin/python3
+"""defines a route for an API endpoint"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
+    """return the status of the API"""
     return jsonify({"status": "ok"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    stats = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+    return jsonify(stats)
