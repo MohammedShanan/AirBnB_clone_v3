919001bce31ff5e6ff58233aa42af70b5db57e89
mohammedshanan <mohammedshana12@gmail.com>
2024-08-04 21:55:54 +0300

    fixed index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,7 +14,7 @@ from models.user import User
 @app_views.route("/status", methods=["GET"])
 def get_status():
     """return the status of the API"""
-    return jsonify({"status": "ok"})
+    return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats/", methods=["GET"])
