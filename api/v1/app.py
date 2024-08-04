c985258ac52879b5d99956e423fb3b515b05e4f1
mohammedshanan <mohammedshana12@gmail.com>
2024-08-04 21:53:45 +0300

    fixed index

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ a Flask application for an API server """
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """closes the storage on teardown"""
+    """Close storage"""
     storage.close()
 
 
@@ -22,6 +22,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(getenv("HBNB_API_PORT", "5000"))
+    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(os.getenv("HBNB_API_PORT", "5000"))
     app.run(host=host, port=port, threaded=True)
